22994df034a9ca76f7d0bc787e21033870d1a0eb
Tanishka Verma <tanishkaverma43@gmail.com>
2025-05-03 19:29:52 +0530

    final commit

--- index.js
+++ index.js
@@ -7,10 +7,18 @@ const path = require("path");
 const cors = require("cors");
 const port = process.env.PORT || 4000;
 
-app.use(express.json());
+/*app.use(express.json());
 app.use(cors({
-  origin: ['https://jewellery-shop-frontend.vercel.app/','https://jewellery-shop-admin.vercel.app/listproduct'],
-}));
+  origin: ['https://jewellery-shop-frontend.vercel.app','https://jewellery-shop-admin.vercel.app/listproduct'],
+  methods: ["GET", "POST", "PUT", "PATCH", "UPDATE", "DELETE"],
+  credentials: true,
+}));*/
+//https://jewellery-shop-frontend.vercel.app/login
+
+
+app.use(express.json());
+app.use(cors());
+
 
 // Database Connection With MongoDB
 mongoose.connect("mongodb+srv://tanishkaverma43:89gnJR38dWbxuslu@cluster0.amcsipm.mongodb.net/e-commercee");
